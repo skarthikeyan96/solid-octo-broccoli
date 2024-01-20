858c71dae0998289147adb5a3c64e8b826a29e16
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-01-20 08:27:36 +0530

    fix: attempt #4

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import fs from 'fs'
-import { ChildProcess } from 'child_process';
+import { execSync } from 'child_process';
 
 async function run() {
   const GITHUB_REPOSITORY =
     "https://github.com/skarthikean96/solid-octo-broccoli";
 
-  const commitHash = ChildProcess.execSync("git rev-parse HEAD").toString().trim();
+  const commitHash = execSync("git rev-parse HEAD").toString().trim();
 
   const repoOwner = GITHUB_REPOSITORY.split("/")[0];
   const repoName = GITHUB_REPOSITORY.split("/")[1];
